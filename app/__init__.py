146553c29ffefa8ee7f8fdff2a8ad6c1855a4c5c
Abdelhadi Dyouri <raryat@gmail.com>
2022-07-17 13:56:36 +0100

    Step 3 — Creating a Flask Application Factory

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+from config import Config
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Initialize Flask extensions here
+
+    # Register blueprints here
+
+    @app.route('/test/')
+    def test_page():
+        return '<h1>Testing the Flask Application Factory Pattern</h1>'
+
+    return app
